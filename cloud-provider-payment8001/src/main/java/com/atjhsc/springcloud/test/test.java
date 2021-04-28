41fbff3b1c3d32f12113f07a712cf0b4ad43648d
lxj <2186029151@qq.com>
2021-04-28 21:33:02 +0800

    init

---
+++ cloud-provider-payment8001/src/main/java/com/atjhsc/springcloud/test/test.java
@@ -0,0 +1,10 @@
+package com.atjiuhua.springcloud.test;
+
+import com.atjiuhua.springcloud.entities.CommonRusult;
+
+public class test {
+    public static void main(String[] args) {
+        CommonRusult commonRusult=new CommonRusult();
+
+    }
+}
